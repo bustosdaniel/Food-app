7ffb58fa06c271490b9dd198ff265889a8e215f3
Daniel <bustosneifer@gmail.com>
2022-09-20 11:31:25 -0500

    added catalogs

---
+++ src/catalogs/index.js
@@ -0,0 +1,17 @@
+export const Section = {
+  'our-foods': 'Our foods',
+  burgers: 'Burgers',
+  breads: 'Breads',
+  sandwiches: 'Sandwiches',
+  drinks: 'Drinks',
+  pizzas: 'Pizzas',
+  'best-foods': 'Best foods',
+  bbqs: 'Bbqs',
+  chocolates: 'Chocolates',
+  desserts: 'Desserts',
+  'fried-chicken': 'Fried chicken',
+  'ice-cream': 'Ice cream',
+  porks: 'Porks',
+  sausages: 'Sausages',
+  steaks: 'Steaks'
+}
